3261241ec985fe4c48e6d5708a0624b885bdd7cc
kavija <karthickeyan.v03@gmail.com>
2018-05-17 18:15:02 -0500

    Update Detailed.js

--- src/js/pages/Detailed.js
+++ src/js/pages/Detailed.js
@@ -6,7 +6,7 @@ import RelatedVideos from '../components/Search/video_related';//Import the Vide
 import SearchBar from '../components/Search/search_bar';//Import the SearchBar component from ./components/search_bar.js
 
 //const(constant) is part of ES6
-const API_KEY = 'AIzaSyCPCtBSZuB49F1-9Xt3DBS6PhePkm5j50E'; //youtube API Key
+const API_KEY = 'Youtube-API-Key'; //youtube API Key
 
 export default class Detailed extends Component {
     constructor(props) {
