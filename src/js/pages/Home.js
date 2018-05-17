a2e7e55d1349c1724ad598b0222f8ca8c12837f9
kavija <karthickeyan.v03@gmail.com>
2018-05-17 16:46:19 -0500

    Update Home.js

--- src/js/pages/Home.js
+++ src/js/pages/Home.js
@@ -11,7 +11,7 @@ import * as HomeActions from "../actions/homeActions";
 import HomeVideos from "../components/Home/HomeVideos";
 
 //const(constant) is part of ES6
-const API_KEY = 'AIzaSyCPCtBSZuB49F1-9Xt3DBS6PhePkm5j50E'; //youtube API Key
+const API_KEY = 'AIzaSyCPCtBSZuB49F1-9Xt3DBS6P'; //youtube API Key
 
 export default class Home extends Component {
     constructor(props) {
